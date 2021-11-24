7177d074966f3446c260e0480433e000602d419e
jessekim-ck <jessekim.ck.94@gmail.com>
2021-11-24 23:20:56 +0900

    deal with inputs less than one.

--- week3/square_root.py
+++ week3/square_root.py
@@ -1,4 +1,16 @@
-def sqrt(y, eps=10**(-5)):
+def sqrt(y, eps=10**(-8)):
+
+    # y가 1보다 크다고 상정하고 제곱근을 구할 예정
+    # 따라서 y가 1보다 작다면 역수를 취해준다.
+    if y < 0:
+        raise RuntimeError("y should be bigger than 0.")
+    elif y < 1:
+        lt_one = True
+        original_y = y
+        y = 1 / y
+    else:
+        lt_one = False
+
     num = y  # 제곱근이 될 변수
     upper_bound = y  # 제곱근의 upper bound
     lower_bound = 0  # 제곱근의 lower bound
@@ -15,5 +27,16 @@ def sqrt(y, eps=10**(-5)):
 
         # num을 upper_bound와 lower_bound의 평균으로 업데이트
         num = (upper_bound + lower_bound) / 2
+
+    # 원래 y가 1보다 작았다면, 역수의 제곱근에 원래 y를 곱해줘야 제곱근이 됨.
+    if lt_one:
+        num = num * original_y
     
     return num
+
+
+print(sqrt(0.01))
+print(sqrt(0.3))
+print(sqrt(2))
+print(sqrt(13))
+print(sqrt(400))
